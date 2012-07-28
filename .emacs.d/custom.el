4ef5fa720a52dc8431453ad5fcbcc87b4662f678
Seyi Ogunyemi <micrypt@gmail.com>
2012-07-28 08:13:46 +0100

    Updated emacs config

---
+++ .emacs.d/custom.el
@@ -0,0 +1,10 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(global-font-lock-mode t)
+(setq font-lock-maximum-decoration t)
+(setq-default ispell-program-name "/usr/local/bin/aspell")
+
+(color-theme-molokai)
